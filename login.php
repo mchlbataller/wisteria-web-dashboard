afbf1bf44546b4d28ee2a0a0c252a669238ec0e3
mikaeruu <mchlbataller@gmail.com>
2019-10-06 07:58:33 +0800

    new dashboard for members

--- login.php
+++ login.php
@@ -1,27 +1,63 @@
-<?php 
-    require_once "assets/conn.php";
-    session_start();
-    $sql = new mysqli($host, $user, $pw, $db);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>WWD, Inc.</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <a href="index.html" class="navbar-brand font-weight-light">WWD Inc.</a>
 
-    // check the sql database if there are errors
-    if($sql->connect_error)
-        die("Access Error: " . $sql->connect_error);
+        <!-- Toggler/collapsible Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    $user = $_POST["username"];
-    $pw = $_POST["pw"];
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="main-navigation">
+            <ul class="navbar-nav font-weight-bold">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php#about">About Us</a>
+                </li>
+                <li class="nav-item bg-primary">
+                    <a class="nav-link" href="login.html">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-    $query = $sql->query("select * from users where username='$user' and password='$pw'");
-    
-    if($query->num_rows > 0)
-    {
-        echo "You now have access to the game.";
-    }
-    else
-    {
-        $_SESSION['wrong'] = true;
-        echo <<<END
-            <script>
-                window.location.replace("/");
-            </script>
-        END;
-    }
+    <div class="container main">
+        <div class="login padded">
+            <h4 class="text-center">Log In</h4><hr>
+            <div class="text-center">
+                <p class="text-white bg-danger">
+                <?php
+                if(isset($_GET['login']))
+                    if($_GET['login'] == "error")
+                        echo "Wrong username/password!";
+                    else echo "Registration error. Please contact your system administrator.";
+                ?>
+                </p>
+                <form method="post" action="login-process.php">
+                    <div class="form-group">
+                        Username:<br>
+                        <input type="text" name="username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        Password:<br>
+                        <input type="password" name="pw" placeholder="Password">
+                    </div>
+                    <input type="submit" class="btn btn-outline-primary submit">
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,11 +1,11 @@
 <?php
 if(!empty($_POST["register-user"])) {
     require_once "assets/conn.php";
-
+    session_start();
     /* Form Required Field Validation */
     foreach($_POST as $key=>$value) {
         if(empty($_POST[$key])) {
-            $error_message = "All Fields are required";
+            die("All fields are required.");
             break;
         }
     }
@@ -13,7 +13,6 @@ if(!empty($_POST["register-user"])) {
     /* Email Validation */
     if(!isset($error_message)) {
         if (!filter_var($_POST["userEmail"], FILTER_VALIDATE_EMAIL)) {
-        $error_message = "Invalid Email Address";
         }
     }
 
@@ -25,16 +24,26 @@ if(!empty($_POST["register-user"])) {
         $lastname = $_POST["lastName"];
         $middleName = $_POST["middleName"];
         $email = $_POST["userEmail"];
-        $query = "INSERT INTO users (username, first_name, last_name, middle_name, password, email) VALUES('$username', '$firstname', '$lastname', '$middleName', 'md5($password)', '$email')";
+        $query = "INSERT INTO users (username, first_name, last_name, middle_name, password, email) VALUES('$username', '$firstname', '$lastname', '$middleName', '$password', '$email')";
         $result = $db_handle->query($query);
         if(!empty($result)) {
             $error_message = "";
             $success_message = "You have registered successfully!";    
             unset($_POST);
+            echo <<<END
+                <script>
+                    window.location.replace("index.php?reg=success");
+                </script>
+            END;
         } else {
-            $error_message = "Problem in registration. Try Again!";    
-        }
+            $error_message = "Problem in registration. Try Again!";
+            echo <<<END
+                <script>
+                    window.location.replace("index.php?reg=error");
+                </script>
+            END;
 
+        }
     }
 }
 ?>
